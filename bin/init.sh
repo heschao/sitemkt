401b27436479f5efa0e5ff0c73bc36b033f47456
Chao Chen <you@me.com>
2018-08-21 14:47:58 -0600

    added test html file

--- bin/init.sh
+++ bin/init.sh
@@ -1,14 +1,4 @@
 
-docker-compose -f docker/docker-compose.yml down --remove-orphans
-
-sleep 3
-
 docker build . -t sitemkt -f docker/Dockerfile
-
-docker-compose -f docker/docker-compose.yml up -d
-
-# create tables
-docker-compose exec sitemkt-container python bin/main.py init-db
-
 
 
